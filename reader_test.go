d5616098e8c6759b20bd60b332ca597c5795b9bf
zpatrick <zpatrick319@hotmail.com>
2017-03-02 04:31:36 +0000

    add test placeholders

---
+++ reader_test.go
@@ -0,0 +1,14 @@
+package rclient
+
+import(
+	"testing"
+)	
+
+func TestJSONReaderFactory(t *testing.T){
+	t.Skip("todo")
+}
+
+func TestJSONReaderFactoryErrors(t *testing.T){
+        t.Skip("todo")
+}
+
---
+++ sender_test.go
@@ -0,0 +1,14 @@
+package rclient
+
+import(
+	"testing"
+)	
+
+func TestJSONSenderFactory(t *testing.T){
+	t.Skip("todo")
+}
+
+func TestJSONSenderFactoryErrors(t *testing.T){
+        t.Skip("todo")
+}
+
